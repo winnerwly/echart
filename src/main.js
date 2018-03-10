e219f6b796525611bdd355ee1f606d4eb185dc42
winnerwly <975080391@qq.com>
2018-03-10 23:18:36 +0800

    ..

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// import echarts from 'echarts'
+
+// Vue.prototype.$echarts = echarts 
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
